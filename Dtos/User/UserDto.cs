4693cba735d4e4fe5e90355927e843620704f6b1
Marlon <marlondorian34@gmail.com>
2026-08-06 00:15:38 -0600

    fix: Los Users ahora retornan sus roles

--- Dtos/User/UserDto.cs
+++ Dtos/User/UserDto.cs
@@ -6,6 +6,7 @@ namespace AutoCare_Club_Api.Dtos.User
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = new();
         public DateTime CreatedAt { get; set; }
     }
 }
